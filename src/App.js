662ccfa1ce350e07e77ed130f6bd8e1b9136336b
Raymond Doetjes <rdoetjes@phonax.com>
2022-03-31 20:23:50 +0200

    reset

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Topic from './Topic.js'
 
 function AppHeader() {
+
   const playMe = (soundFile) =>{
     let sound = new Audio(process.env.PUBLIC_URL + '/' +soundFile);
     sound.play();
@@ -12,21 +13,21 @@ function AppHeader() {
       <div className='grid'>
         <div className='section'></div>
         <div className='section'>
-        <Topic topic="KILL" image="kill.png" onClick={() => {playMe("chainsaw.mp3")}}/>
+        <Topic topic="KILL" image="kill.png" onClick={() => {playMe("gunshot.mp3")}}/>
         </div>
         <div className='section'></div>
 
         <div className='section'>
-        <Topic topic="FUCK!" image="fuck.png"/>
+        <Topic topic="FUCK!" image="fuck.png" onClick={() => {playMe("orgasm.mp3")}}/>
         </div>
-        <div className='section'><img className="box" alt="kill" src={process.env.PUBLIC_URL + '/rutte.jpeg'} /> </div>
+        <div className='section'><img id="idiot" className="box" alt="kill" src={process.env.PUBLIC_URL + '/idiots/rutte.jpeg'} /> </div>
         <div className='section'>
-        <Topic topic="MARRY" image="marry.png"/>
+        <Topic topic="MARRY" image="marry.png" onClick={() => {playMe("sucker.mp3")}} />
         </div>
 
         <div className='section'></div>
         <div className='section'>
-        <Topic topic="DUMP" image="dump.png"/>
+        <Topic topic="DUMP" image="dump.png" onClick={() => {playMe("dumped.mp3")}}/>
         </div>
         <div className='section'></div>
       </div>
