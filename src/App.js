39495c4af753e64ac4f025a24ac92fff9c4074cf
Raymond Doetjes <rdoetjes@phonax.com>
2022-04-02 09:37:37 +0200

    converted App.js to Coponent class

--- src/App.js
+++ src/App.js
@@ -1,65 +1,78 @@
-import React, {useState} from 'react'
+import React, {Component, setState} from 'react'
 import './App.css';
 import Topic from './Topic.js'
 import {celebs} from './celebs.js'
-
-function AppHeader() {
+import mqtt from 'mqtt'
+class AppHeader extends Component {
   
-  const [newCeleb, setNewCeleb] = useState(celebs[Math.floor(Math.random() * celebs.length)])
-  const [alreadyHad, addCeleb] = useState([])
-  const [isDisabled, setDisabled] = useState(false)
 
-  function getCeleb(celebs, alreadyHad) {  
-    if (alreadyHad && !alreadyHad.includes(newCeleb))
-      addCeleb(alreadyHad => [...alreadyHad, newCeleb])
+  constructor(props){
+    super(props);
+    this.state = {
+      newCeleb: celebs[Math.floor(Math.random() * celebs.length)],
+      alreadyHad: [],
+      isDisabled: false
+    }
+  }
+
+  componentDidMount(){
+
+  }
+
+  getCeleb(celebs) {  
+    if (this.state.alreadyHad && !this.state.alreadyHad.includes(this.state.newCeleb))
+      this.state.alreadyHad.push(this.state.newCeleb)
  
     let tempCeleb = celebs[Math.floor(Math.random() * celebs.length)]
     
-    if (alreadyHad && alreadyHad.length >= celebs.length){
-      setDisabled(true)
+    if (this.state.alreadyHad && this.state.alreadyHad.length >= celebs.length){
+      this.setState({isDisabled: true});
       return "done.jpeg"
     }
 
-    while (alreadyHad && alreadyHad.includes(tempCeleb))
+    while (this.state.alreadyHad && this.state.alreadyHad.includes(tempCeleb))
       tempCeleb = celebs[Math.floor(Math.random() * celebs.length)]
     
-    addCeleb(alreadyHad => [...alreadyHad, tempCeleb])
+    this.state.alreadyHad.push(tempCeleb);
 
     return tempCeleb
   }
 
-  const playMe = (soundFile, celebs, alreadyHad) =>{
+  playMe(soundFile, celebs, alreadyHad){
     let sound = new Audio(process.env.PUBLIC_URL + '/' +soundFile);
     sound.play();
-    setNewCeleb(getCeleb(celebs, alreadyHad))
+    let tempCeleb = this.getCeleb(celebs, alreadyHad);
+    this.setState({newCeleb: tempCeleb});
   }
 
+  render(){
   return (
-    <div className='KFMD'>
-      <div className='grid'>
-        <div className='section'></div>
-        <div className='section'>
-          <Topic topic="KILL" image="kill.png" disabled={isDisabled} onClick={() => {playMe("gunshot.mp3", celebs, alreadyHad)}}/>
-        </div>
-        <div className='section'></div>
+      <div className='KFMD'>
+        <div className='grid'>
+          <div className='section'></div>
+          <div className='section'>
+            <Topic topic="KILL" image="kill.png" disabled={this.state.isDisabled} onClick={() => {this.playMe("gunshot.mp3", celebs, this.state.alreadyHad)}}/>
+          </div>
+          <div className='section'></div>
 
-        <div className='section'>
-          <Topic topic="FUCK!" image="fuck.png" disabled={isDisabled} onClick={() => {playMe("orgasm.mp3", celebs, alreadyHad)}}/>
-        </div>
-        <div className='section'><img id="idiot" className="box" alt="kill" src={process.env.PUBLIC_URL + "/celebs/" + newCeleb} /> </div>
-        <div className='section'>
-          <Topic topic="MARRY" image="marry.png" disabled={isDisabled} onClick={() => {playMe("sucker.mp3", celebs, alreadyHad)}} />
-        </div>
+          <div className='section'>
+            <Topic topic="FUCK!" image="fuck.png" disabled={this.state.isDisabled} onClick={() => {this.playMe("orgasm.mp3", celebs, this.state.alreadyHad)}}/>
+          </div>
+          <div className='section'><img id="idiot" className="box" alt="kill" src={process.env.PUBLIC_URL + "/celebs/" + this.state.newCeleb} /> </div>
+          <div className='section'>
+            <Topic topic="MARRY" image="marry.png" disabled={this.state.isDisabled} onClick={() => {this.playMe("sucker.mp3", celebs, this.state.alreadyHad)}} />
+          </div>
 
-        <div className='section'></div>
-        <div className='section'>
-          <Topic topic="DUMP" image="dump.png" disabled={isDisabled} onClick={() => {playMe("dumped.mp3", celebs, alreadyHad)}}/>
+          <div className='section'></div>
+          <div className='section'>
+            <Topic topic="DUMP" image="dump.png" disabled={this.state.isDisabled} onClick={() => {this.playMe("dumped.mp3", celebs, this.state.alreadyHad)}}/>
+          </div>
+          <div className='section'></div>
         </div>
-        <div className='section'></div>
+      
       </div>
-    
-    </div>
-  );
+    );
+  }
 }
 
 export default AppHeader;
