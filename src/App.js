219cc94fb1ea1e3d15f40f0039863f1c918ba9fa
Raymond Doetjes <rdoetjes@phonax.com>
2022-04-02 10:47:50 +0200

    mqtt event handling works

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Topic from './Topic.js'
 import {celebs} from './celebs.js'
 import mqtt from 'mqtt'
+import { toHaveFocus } from '@testing-library/jest-dom/dist/matchers';
 class AppHeader extends Component {
   
 
@@ -16,7 +17,8 @@ class AppHeader extends Component {
   }
 
   componentDidMount(){
-    this.client = mqtt.connect("wss://test.mosquitto.org:8081");
+    //todo make a wss (ssl)
+    this.client = mqtt.connect("ws://192.168.178.77:9001");
 
     this.client.on("connect", ()   => {
       console.log("connected");
@@ -24,8 +26,45 @@ class AppHeader extends Component {
     });
 
     this.client.on("message", (topic, message) => {
-      console.log(message.toString());
+      this.callEvent(message.toString())
     });
+  }
+
+  kill(){
+    this.playMe("gunshot.mp3", celebs, this.state.alreadyHad);
+  }
+
+  fuck(){
+    this.playMe("orgasm.mp3", celebs, this.state.alreadyHad)
+  }
+
+  dump(){
+    this.playMe("dumped.mp3", celebs, this.state.alreadyHad);
+  }
+
+  marry(){
+    this.playMe("sucker.mp3", celebs, this.state.alreadyHad);
+  }
+
+  callEvent(brokerMessage){
+    if (this.state.isDisabled)
+      return;
+    
+    var message= brokerMessage.toUpperCase();
+
+    if (message === "UP" || message ==="U" || message === "K" || message === "KILL")
+      this.kill();
+
+    if (message === "DOWN" || message ==="D" || message === "DUMP")
+     this.dump();
+    
+    if (message === "LEFT" || message ==="L" || message === "F" || message === "FUCK")
+      this.fuck();
+    
+    if (message === "RIGHT" || message ==="R" || message === "M" || message === "MARRY")
+    this.marry()
+
+    console.log(message.toString());
   }
 
   getCeleb(celebs) {  
@@ -49,6 +88,7 @@ class AppHeader extends Component {
 
   playMe(soundFile, celebs, alreadyHad){
     let sound = new Audio(process.env.PUBLIC_URL + '/' +soundFile);
+    sound.muted = false;
     sound.play();
     let tempCeleb = this.getCeleb(celebs, alreadyHad);
     this.setState({newCeleb: tempCeleb});
@@ -60,21 +100,21 @@ class AppHeader extends Component {
         <div className='grid'>
           <div className='section'></div>
           <div className='section'>
-            <Topic topic="KILL" image="kill.png" disabled={this.state.isDisabled} onClick={() => {this.playMe("gunshot.mp3", celebs, this.state.alreadyHad)}}/>
+            <Topic topic="KILL" image="kill.png" disabled={this.state.isDisabled} onClick={() => {this.kill()}}/>
           </div>
           <div className='section'></div>
 
           <div className='section'>
-            <Topic topic="FUCK!" image="fuck.png" disabled={this.state.isDisabled} onClick={() => {this.playMe("orgasm.mp3", celebs, this.state.alreadyHad)}}/>
+            <Topic topic="FUCK!" image="fuck.png" disabled={this.state.isDisabled} onClick={() => {this.fuck()}}/>
           </div>
           <div className='section'><img id="idiot" className="box" alt="kill" src={process.env.PUBLIC_URL + "/celebs/" + this.state.newCeleb} /> </div>
           <div className='section'>
-            <Topic topic="MARRY" image="marry.png" disabled={this.state.isDisabled} onClick={() => {this.playMe("sucker.mp3", celebs, this.state.alreadyHad)}} />
+            <Topic topic="MARRY" image="marry.png" disabled={this.state.isDisabled} onClick={() => {this.marry()}} />
           </div>
 
           <div className='section'></div>
           <div className='section'>
-            <Topic topic="DUMP" image="dump.png" disabled={this.state.isDisabled} onClick={() => {this.playMe("dumped.mp3", celebs, this.state.alreadyHad)}}/>
+            <Topic topic="DUMP" image="dump.png" disabled={this.state.isDisabled} onClick={() => {this.dump()}}/>
           </div>
           <div className='section'></div>
         </div>
